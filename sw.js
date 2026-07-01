86e39278867dd6bd0206af3d456a16f0680e88e7
Kyanna11 <2604536841@qq.com>
2026-07-01 22:02:22 +0800

    Create  sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheKey = "myhome-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./C2DD4F6E-7577-4855-9927-11E6249175F6.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(cacheKey).then(c => c.addAll(assets)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(list => Promise.all(
+    list.filter(k => k !== cacheKey).map(k => caches.delete(k))
+  )));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
